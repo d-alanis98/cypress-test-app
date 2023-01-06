2ecd600150c645815460871b473cab6e81be1d2a
Damian Alanis <damian.alanis98@gmail.com>
2023-01-06 17:58:16 -0600

    Find and parents overview

--- cypress/e2e/firstTest.spec.ts
+++ cypress/e2e/firstTest.spec.ts
@@ -1,14 +1,16 @@
 /// <reference types="cypress" />
 
 describe('First test suite', () => {
-    it('First test', () => {
+
+    beforeEach(() => {
         // Navigate to the root path
         cy.visit('/');
         // Look for the element containing the Forms text and click on it (it will be the sidebar dropdown of the Forms section)
         cy.contains('Forms').click();
         // Look for the element containing the Form Layouts text and click on it to navigate to the Form Layouts page
-        
         cy.contains('Form Layouts').click();
+    });
+    it('First test', () => {
         // Search element by tag name
         cy.get('input');
         // Find element by ID
@@ -29,6 +31,25 @@ describe('First test suite', () => {
         cy.get('input[placeholder="Email"]#inputEmail1.input-full-width');
         // The recommended way by cypress
         cy.get('[data-cy="imputEmail1"]');
+    });
+
+    it('Second test', () => {
+        cy.get('[data-cy="signInButton"]');
+        cy.contains('Sign in');
+        // When there could be multiple matches for the provided value to search, we can specify extra filters
+        cy.contains('[status="warning"]', 'Sign in');
+        
+        // To select a sibling of an element with an identifier, we can get the reference to the parent 
+        // In this example, we will get the button that is inside the same form as the inputEmail3 element
+        // Difference between find and get is that get is global, so it will match all the buttons in the DOM, while find is relative to the current node
+        cy.get('#inputEmail3')
+            .parents('form')
+            .find('button')
+            .should('contain', 'Sign in')
+            .parents('form')
+            .find('nb-checkbox');
+
+        cy.contains('nb-card', 'Horizontal form').find('[type="email"]');
     });
 });
 
